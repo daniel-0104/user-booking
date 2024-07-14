6a127dc812787904ccd0b2764a154473c9579312
Daniel <122777459+daniel-0104@users.noreply.github.com>
2024-07-14 17:10:39 +0630

    14NoonCommit

--- script.js
+++ script.js
@@ -9,17 +9,62 @@ navLinks.forEach(link => {
 });
 // href clicked active link end
 
-
+// ......................................................feature trailer swiper start ............................
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 2,
   spaceBetween: 30,
   loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  grabCursor: true,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
   },
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    770: {
+        slidesPerView: 2,
+    },
+    1400: {
+        slidesPerView: 3,
+    }
+  }
+});
+// ......................................................feature trailer swiper end ............................
+
+
+const customSelect = document.querySelector(".custom-select");
+const selectBtn = document.querySelector(".select-button");
+
+const selectedValue = document.querySelector(".selected-value");
+const optionsList = document.querySelectorAll(".select-dropdown li");
+
+// add click event to select button
+selectBtn.addEventListener("click", () => {
+  // add/remove active class on the container element
+  customSelect.classList.toggle("active");
+  // update the aria-expanded attribute based on the current state
+  selectBtn.setAttribute(
+    "aria-expanded",
+    selectBtn.getAttribute("aria-expanded") === "true" ? "false" : "true"
+  );
+});
+
+optionsList.forEach((option) => {
+  function handler(e) {
+    // Click Events
+    if (e.type === "click" && e.clientX !== 0 && e.clientY !== 0) {
+      selectedValue.textContent = this.children[1].textContent;
+      customSelect.classList.remove("active");
+    }
+    // Key Events
+    if (e.key === "Enter") {
+      selectedValue.textContent = this.textContent;
+      customSelect.classList.remove("active");
+    }
+  }
+
+  option.addEventListener("keyup", handler);
+  option.addEventListener("click", handler);
 });
