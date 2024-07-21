b48cb794b4760ec8276705475f211a3a28ae6418
Daniel <122777459+daniel-0104@users.noreply.github.com>
2024-07-22 00:18:21 +0630

    22 night commit

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ var swiper = new Swiper(".mySwiper2", {
   spaceBetween: 30,
   slidesPerView: 4,
   autoplay: {
-    delay: 4000,
+    delay: 3000,
     disableOnInteraction: false,
   },
   pagination: {
