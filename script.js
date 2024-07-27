2062efde519d3599411c999bec528fc3bd78b89c
Daniel <122777459+daniel-0104@users.noreply.github.com>
2024-07-27 20:15:06 +0630

    url update

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // href clicked active link start 
-const currentHTMLPage = window.location.href;
-const navLinks = document.querySelectorAll('.nav-link');
-navLinks.forEach(link => link.classList.remove('active'));
-navLinks.forEach(link => {
-  if (currentHTMLPage.includes(link.getAttribute('href'))) {
-    link.classList.add('active');
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  const currentHTMLPage = window.location.pathname;
+  const navLinks = document.querySelectorAll('.nav-link');
+  navLinks.forEach(link => link.classList.remove('active'));
+
+  navLinks.forEach(link => {
+    const linkPath = new URL(link.href).pathname;
+    if (currentHTMLPage === linkPath) {
+      link.classList.add('active');
+    }
+  });
 });
 // href clicked active link end
 
